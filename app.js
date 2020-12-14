746d5d28f803e8190c5ec64795e2788d3aea803f
bestie06 <fatih.kgokmen@gmail.com>
2020-12-14 01:08:06 -0500

    implemented files and bootsrap

---
+++ app.js
@@ -0,0 +1,13 @@
+const express =require('express');
+const bodyParser = require("bodyParser");
+const request= require ("request");
+
+
+
+
+const app=expres();
+
+
+app.listen(3000,function(){
+
+})
