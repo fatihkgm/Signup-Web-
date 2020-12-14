e145c11a5ed6ab406b872be32151f55d5facf0f7
bestie06 <fatih.kgokmen@gmail.com>
2020-12-14 02:14:57 -0500

    update for styles

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const request= require ("request");
 
 
 
-const app=expres();
+const app=express();
+app.get("/", function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+});
 
 
 app.listen(3000,function(){
 
-})
+});
